7fa8f8d18684b0d98fcca389927f1214c778edee
thienlongtpct <thienlongtpct@gmail.com>
2021-07-23 20:10:36 +0300

    first init

--- src/main/resources/static/js/index.js
+++ src/main/resources/static/js/index.js
@@ -1,60 +1,57 @@
-// window.onload = () => {
-//     let canvas = document.getElementById('canvas');
-//     canvas.width = 650;
-//     canvas.height = 650;
-//
-//     let context = canvas.getContext('2d');
-//     let image = new Image();
-//     image.onload = () => context.drawImage(image, 0, 0, 650, 650);
-//     image.src = '../images/areas.png';
-//
-//     const getPosition = (event) => {
-//         let bound = canvas.getBoundingClientRect();
-//         let R = 250;
-//         let x = (event.clientX - bound.left - canvas.clientLeft - canvas.width / 2) / R;
-//         let y = -(event.clientY - bound.top - canvas.clientTop - canvas.height / 2) / R;
-//         return {x, y};
-//     }
-//
-//     canvas.addEventListener('mousemove', event => {
-//         let current = document.getElementById('current');
-//         let {x, y} = getPosition(event);
-//         current.innerText = 'Current Point: (' + (Math.round(x * 100) / 100).toFixed(2) + 'R, ' + (Math.round(y * 100) / 100).toFixed(2) + 'R)';
-//     });
-//
-//     canvas.addEventListener('click', event => {
-//         let selected = document.getElementById('selected');
-//         let newSelected = document.createElement('li');
-//         let {x, y} = getPosition(event);
-//
-//         let bound = canvas.getBoundingClientRect();
-//
-//         let url = "http://localhost:8080/api/checkInside?x=" + x.toString() + '&y=' + y.toString();
-//         fetch(url)
-//             .then(response => {
-//                 if (response.status === 200) return response.json();
-//                 return new Error(response.statusText);
-//             })
-//             .then(result => {
-//                 newSelected.innerHTML =
-//                     '(' + (Math.round(x * 100) / 100).toFixed(2) + 'R, ' + (Math.round(y * 100) / 100).toFixed(2) + 'R): ' +
-//                     (result ? '<span style="color: green">inside</span>' : '<span style="color: red">outside</span>');
-//
-//                 if (result) {
-//                     let plus = new Image();
-//                     plus.onload = () => context.drawImage(plus, event.clientX - bound.left - canvas.clientLeft - 10, event.clientY - bound.top - canvas.clientTop - 10, 20, 20);
-//                     plus.src = '../images/plus.png';
-//                 } else {
-//                     let cross = new Image();
-//                     cross.onload = () => context.drawImage(cross, event.clientX - bound.left - canvas.clientLeft - 10, event.clientY - bound.top - canvas.clientTop - 10, 20, 20);
-//                     cross.src = '../images/cross.png';
-//                 }
-//
-//                 selected.appendChild(newSelected);
-//             })
-//             .catch(error => {
-//                 alert(error);
-//             })
-//
-//     });
-// }
+window.onload = () => {
+    //init board and slider
+    let board = JXG.JSXGraph.initBoard('box1', {
+        axis: true,
+        boundingbox: [-5.25, 5.25, 5.25, -5.25]
+    });
+    let r = board.create('slider', [[1, 3], [3, 3], [1, 3, 3]], {name:'R', snapWidth: 0.5});
+
+    //function generate value
+    let R = () => r.Value();
+    let halfR = () => r.Value()/2;
+    let minusR = () => -r.Value();
+
+    //first corner (triangle)
+    board.create('polygon', [[0, 0], [0, halfR], [R, 0]], {
+        fillColor: '#e74c3c',
+        strokeColor: 'none',
+        withLines: false,
+        vertices: {visible: false}
+    });
+
+    //second corner (square)
+    board.create('polygon', [[0, 0], [R, 0], [R, minusR], [0, minusR]], {
+        fillColor: '#3498db',
+        strokeColor: 'none',
+        withLines: false,
+        vertices: {visible: false}
+    });
+
+    //fourth corner (quarter circle)
+    let quarterCircle = X => Math.sqrt((r.Value()-X)*(r.Value()+X));
+    board.create('functiongraph', [quarterCircle, minusR,  0], {
+        fillColor: '#16a085',
+        fillOpacity: 0.3,
+        strokeColor: 'none',
+        highlight: false,
+        withLines: false,
+        vertices: {visible: false}
+    });
+    board.create('polygon', [[minusR, 0], [0, 0], [0, R]], {
+        fillColor: '#16a085',
+        strokeColor: 'none',
+        withLines: false,
+        vertices: {visible: false}
+    });
+
+    document.getElementById('box1').addEventListener('mousemove', (e, i) => {
+        let currentPos = board.getCoordsTopLeftCorner(e, i),
+            absolutePos = JXG.getPosition(e, i),
+            dx = absolutePos[0]-currentPos[0],
+            dy = absolutePos[1]-currentPos[1];
+
+        let jxgCoordinate = new JXG.Coords(JXG.COORDS_BY_SCREEN, [dx, dy], board);
+        let x = jxgCoordinate.usrCoords[1];
+        let y = jxgCoordinate.usrCoords[2];
+    });
+}
