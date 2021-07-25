63f5103ef004d26d6055666a073a99c1b0fef0b4
thienlongtpct <thienlongtpct@gmail.com>
2021-07-26 01:23:38 +0300

    add point by editor

--- src/main/resources/static/js/index.js
+++ src/main/resources/static/js/index.js
@@ -1,28 +1,10 @@
-const valid = (text) => {
-    let lines = text.split('\n');
-    // return lines.filter(line => line.trim().split(' ').length > 3).length === 0;
-    return true;
-}
+// const valid = (text) => {
+//     let lines = text.split('\n');
+//     return lines.filter(line => line.trim().split(' ').length > 3).length === 0;
+//     return true;
+// }
 
 window.onload = () => {
-    let current = CodeMirror.fromTextArea(document.getElementById('current'), {
-        lineNumbers: true,
-        styleActiveSelected: true,
-        mode: 'text',
-        readOnly: true
-    })
-    current.setSize(null, 30);
-    current.setValue('(0, 0)');
-
-    let input = CodeMirror.fromTextArea(document.getElementById('input'), {
-        styleActiveLine: true,
-        lineNumbers: true,
-        lineWrapping: true,
-        styleActiveSelected: true,
-        mode: 'text'
-    })
-    input.setSize(null, 300);
-    input.setValue('');
     // input.on('change', () => {
     //     input.operation(() => {
     //         let lines = input.getValue().split('\n');
@@ -45,21 +27,88 @@ window.onload = () => {
         //     input.scrollTo(null, after - info.clientHeight + 3);
     // })
 
-    //init board and slider
+    //init board
     let board = JXG.JSXGraph.initBoard('box1',{
         axis: true,
         boundingbox: [-5.25, 5.25, 5.25, -5.25]
     });
 
     //sliders
-    let r = board.create('slider', [[1,3], [3,3], [1, 3, 3]], {name: 'R', snapWidth: 0.5});
-    // let x = board.create('slider', [[1,4], [3,4], [-3, 0, 5]], {name: 'X', snapWidth: 1});
-    // let y = board.create('slider', [[1,5], [3,5], [-5, 0, 5]], {name: 'Y'});
-
-    //function generate value
+    let r = board.create('slider', [[1, 3], [3, 3], [1, 3, 3]], {name: 'R', snapWidth: 0.5});
     let R = () => r.Value();
     let minusR = () => -r.Value();
     let halfR = () => r.Value()/2;
+
+    const addPoint = async (x, y, r, label) => await fetch('http://localhost:8080/api/add?x=' + x + '&y=' + y + '&r=' + r, {
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json',
+        }
+    })
+        .then(response => {
+            if(response.ok){
+                return response.json();
+            }
+            else{
+                alert('Server is not connected!');
+            }
+        })
+        .then(result => {
+            if (label !== '') {
+                board.create('point', [x, y], {
+                    color: result ? 'green' : 'red',
+                    id: label
+                });
+            }
+            else {
+                board.create('point', [x, y], {
+                    color: result ? 'green' : 'red',
+                    label: {visible: false}
+                });
+            }
+        })
+        .catch(error => {
+            alert(error);
+        });
+
+    const getXY = (e, i) => {
+        let currentPos = board.getCoordsTopLeftCorner(e, i),
+            absolutePos = JXG.getPosition(e, i),
+            dx = absolutePos[0] - currentPos[0],
+            dy = absolutePos[1] - currentPos[1];
+
+        let jxgCoordinate = new JXG.Coords(JXG.COORDS_BY_SCREEN, [dx, dy], board);
+        let x = jxgCoordinate.usrCoords[1];
+        let y = jxgCoordinate.usrCoords[2];
+
+        return {x, y};
+    }
+
+    let current = CodeMirror.fromTextArea(document.getElementById('current'), {
+        lineNumbers: true,
+        styleActiveSelected: true,
+        mode: 'text',
+        readOnly: true
+    })
+    current.setSize(null, 30);
+    current.setValue('(0, 0)');
+
+    let input = CodeMirror.fromTextArea(document.getElementById('input'), {
+        styleActiveLine: true,
+        lineNumbers: true,
+        lineWrapping: true,
+        styleActiveSelected: true,
+        mode: 'text'
+    })
+    input.setSize(null, 300);
+    input.setValue('');
+    input.on('keyup', (e, i) => {
+        if (i.code === 'Enter') {
+            let lineIndex = input.getCursor().line-1;
+            let content = input.getValue().split('\n')[lineIndex].trim().split(' ');
+            addPoint(content[0], content[1], r.Value(), content[2]);
+        }
+    });
 
     // first corner (triangle)
     board.create('polygon', [[0,0], [0, halfR], [R,0]],{
@@ -96,64 +145,14 @@ window.onload = () => {
     })
 
     document.getElementById('box1').addEventListener('mousemove', (e, i) => {
-        let currentPos = board.getCoordsTopLeftCorner(e,i),
-            absolutePos = JXG.getPosition(e, i),
-            dx = absolutePos[0] - currentPos[0],
-            dy = absolutePos[1] - currentPos[1];
-
-
-        //console.log(currentPos + ' ' + absolutePos + ' ' + dx + ' ' + dy);
-
-        let jxgCoordinate = new JXG.Coords(JXG.COORDS_BY_SCREEN, [dx, dy], board);
-        let x = jxgCoordinate.usrCoords[1].toFixed(2);
-        let y = jxgCoordinate.usrCoords[2].toFixed(2);
-
-        current.setValue('(' + x + ', ' + y + ')');
+        let {x, y} = getXY(e, i);
+        current.setValue('(' + x.toFixed(2) + ', ' + y.toFixed(2) + ')');
     });
 
     document.getElementById('box1').addEventListener('mousedown', (e,i) => {
-        let currentPos = board.getCoordsTopLeftCorner(e,i),
-            absolutePos = JXG.getPosition(e, i),
-            dx = absolutePos[0] - currentPos[0],
-            dy = absolutePos[1] - currentPos[1];
-
-
-        //console.log(currentPos + ' ' + absolutePos + ' ' + dx + ' ' + dy);
-
-        let jxgCoordinate = new JXG.Coords(JXG.COORDS_BY_SCREEN, [dx, dy], board);
-        let x = jxgCoordinate.usrCoords[1];
-        let y = jxgCoordinate.usrCoords[2];
-
-        //console.log(x);
-
-        //let newURL = window.location.href + "/api/add?x=" + x + "&y=" + y;
-
-        //console.log(newURL);
-        //window.location.href = newURL;
-
-        fetch('http://localhost:8080/api/add?x=' + x + '&y=' + y + '&r=' + r.Value(), {
-            method: 'GET',
-            headers: {
-                'Content-Type': 'application/json',
-            }
-        })
-            .then(response => {
-                if(response.ok){
-                    return response.json();
-                }
-                else{
-                    alert('Server is not connected!');
-                }
-            })
-            .then(result => {
-                board.create('point', [x, y], {
-                    color: result ? 'green' : 'red',
-                    label: {visible: false}
-                });
-            })
+        let {x, y} = getXY(e, i);
+        addPoint(x, y, r.Value(), '');
     });
-
-
 
     let advancedInput = document.getElementById("advanced");
     let simpleInput = document.getElementById("simple");
